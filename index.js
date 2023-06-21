e8c94ce1cbc17ba3b2eaf09ac10ddad288d7cb24
Yahya Jabary <jabaryyahya@gmail.com>
2023-06-22 00:03:38 +0200

    update

--- index.js
+++ index.js
@@ -3,21 +3,38 @@
 const { ArgumentParser } = require('argparse')
 const { version } = require('./package.json')
 
+const perror = (message) => {
+    const stackTrace = process.stack.slice()
+    console.error(`error: ${message}`, stackTrace)
+    process.exit(1)
+}
+
+const assert = (assertion) => {
+    if (!assertion) {
+        perror('assertion failed')
+    }
+}
+
 class ArgParser {
     static #args = null
 
     static parse() {
-        const parser = new ArgumentParser({
-            description: 'notion html export → pretty html',
-        })
-
+        const parser = new ArgumentParser({ description: 'notion html export deobfuscator' })
         parser.add_argument('-v', '--version', { action: 'version', version })
-        parser.add_argument('-f', '--foo', { help: 'foo bar' })
-        parser.add_argument('-b', '--bar', { help: 'bar foo' })
-        parser.add_argument('--baz', { help: 'baz bar' })
+        parser.add_argument('-z', '--zip', { help: 'zip output', action: 'store_true' })
+        parser.add_argument('-s', '--style', { help: 'overwrite notions default css with custom css', action: 'store_true' })
+        parser.add_argument('input', { help: 'path to .zip file containing html export' })
+        ArgParser.#args = parser.parse_args()
+    }
 
-        console.dir(parser.parse_args())
+    static get_args() {
+        assert(ArgParser.#args !== null)
+        return ArgParser.#args
     }
 }
 
-ArgParser.parse()
+async function main() {
+    ArgParser.parse()
+    console.log(ArgParser.get_args())
+}
+main()
