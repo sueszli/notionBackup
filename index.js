49fbb038035bba2ae498b7227920007b371ab1b2
Yahya Jabary <jabaryyahya@gmail.com>
2023-06-21 19:29:28 +0200

    update

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hi')
+'use strict'
+
+const { ArgumentParser } = require('argparse')
+const { version } = require('./package.json')
+
+class ArgParser {
+    static #args = null
+
+    static parse() {
+        const parser = new ArgumentParser({
+            description: 'A Notion HTML export enhancer',
+        })
+
+        parser.add_argument('-v', '--version', { action: 'version', version })
+        parser.add_argument('-f', '--foo', { help: 'foo bar' })
+        parser.add_argument('-b', '--bar', { help: 'bar foo' })
+        parser.add_argument('--baz', { help: 'baz bar' })
+
+        console.dir(parser.parse_args())
+    }
+}
+
+ArgParser.parse()
