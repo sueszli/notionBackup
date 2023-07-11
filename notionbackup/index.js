d6158ff061b01fc22f1157f1c2befcc9fecdc72b
yahya jabary <61852663+sueszli@users.noreply.github.com>
2023-07-11 19:00:11 +0200

    Update index.js

--- notionbackup/index.js
+++ notionbackup/index.js
@@ -25,7 +25,7 @@ class ArgParser {
     }
 
     static parseArgs() {
-        const parser = new ArgumentParser({ description: 'notion html export fixer' })
+        const parser = new ArgumentParser({ description: 'fix notion html exports' })
         parser.add_argument('input', { help: 'path to zip file containing the html export' })
         const args = parser.parse_args()
 
